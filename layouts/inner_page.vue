bf8e1f3df3c9d4615c3cd67fef00b777f686f0e0
OlivierC <coq.olivier36@gmail.com>
2024-11-28 17:11:07 -0500

    Project refresh

--- layouts/inner_page.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <NavBar />
-        <Nuxt/>
-    </div>
-</template>   
--- plugins/index.js
+++
@@ -1,6 +0,0 @@
-export default async({ store }) => {
-    // await store.dispatch('loadFilters')
-    // store.dispatch('loadFilters').then(()=> {
-    //     console.log('loaded filters')
-    // })
-}
