d06a5eed9e3441c4281d8cefeb0c7a943fd3016c
OlivierC <coq.olivier36@gmail.com>
2024-12-13 00:30:42 -0500

    INVISBLE duran duran

--- server/api/contact/send.post.ts
+++ server/api/contact/send.post.ts
@@ -1,6 +1,19 @@
 export default defineEventHandler(async (event) => {
 
   const post_data = await readBody(event);
-  console.log('coming from confirm frontend', post_data, process.env.email_js_key);
+  console.log('coming from confirm frontend', post_data);
   
+  const post_obj = {
+    service_id: process.env.email_js_service_id,
+    template_id: process.env.email_js_template_id,
+    user_id: process.env.email_js_user_id,
+    template_params: {
+      'from_name': post_data.name,
+      'from_email': post_data.email,
+      'message': post_data.message,
+      'to_name': 'Nathan'
+    }
+  }
+
+
 });
