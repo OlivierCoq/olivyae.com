b4747ea35d0f96c860f8bfea0e5b2f38f343220e
OlivierC <coq.olivier36@gmail.com>
2024-12-13 00:20:50 -0500

    git switch

--- server/api/contact/send.post.ts
+++ server/api/contact/send.post.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
 
   const post_data = await readBody(event);
-  console.log('coming from confirm frontend', post_data);
+  console.log('coming from confirm frontend', post_data, process.env.email_js_key);
   
 });
